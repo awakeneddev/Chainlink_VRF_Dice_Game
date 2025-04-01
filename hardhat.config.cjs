4c928c003dd0e77723fb1406891233a00c141009
awakeneddev <awakeneddevs@gmail.com>
2025-04-01 21:13:22 +0545

    config: @openzeppelin/hardhat-upgrades

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
+require("@openzeppelin/hardhat-upgrades");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
@@ -7,6 +8,11 @@ module.exports = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       accounts: [`${process.env.PRIVATE_KEY}`],
+    }
+  },
+  etherscan: {
+    apiKey:{
+      sepolia: `${process.env.ETHER_SCAN_API_KEY}`
     }
   }
 };
